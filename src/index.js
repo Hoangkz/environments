82c115c758ce4ee4fa4c194b206d10b5c47404c0
hoang <hoangyl245@gmail.com>
2022-07-08 17:02:49 +0700

    fix index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT || 3000;
 
 const handlebars = require('express-handlebars');
 const path = require('path');
